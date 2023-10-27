28ec33750ff99856605ae4256241b168bd391f39
katymun <munkate2003@gmail.com>
2023-10-27 14:13:19 +0300

    Added add and delete functions

--- Lab2/src/AppMain.java
+++ Lab2/src/AppMain.java
@@ -7,6 +7,16 @@ public class AppMain {
         Path folderPath = Paths.get("C:\\Users\\munka\\OneDrive\\Documents\\uni-git-projects\\OOP-uni\\testFolder");
         Scanner scanner = new Scanner(System.in);
         String command = "";
+        FileManager fm = new FileManager(folderPath);
         System.out.println("Welcome!");
+
+
+        while (command != "exit") {
+            System.out.println("Choose one of the commands (commit, info <filename>, status, exit): ");
+        }
+    }
+
+    public void printMenu() {
+        System.out.println("Choose one of the commands (commit, info <filename>, status, exit): ");
     }
 }
--- Lab2/src/FileManager.java
+++ Lab2/src/FileManager.java
@@ -1,11 +1,29 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.nio.file.*;
 
 public class FileManager {
-    private List<FileSnapshot> snapshots = new ArrayList<>();
+    private List<FileSnapshot> snapshots;
     private Date lastCommit;
-    private List<String> storedFileNames = new ArrayList<>();
+    private List<String> storedFileNames;
+    Path folderPath;
+
+    public FileManager(Path folderPath) {
+        snapshots = new ArrayList<>();
+        storedFileNames = new ArrayList<>();
+        this.folderPath = folderPath;
+    }
+
+    public Date getLastCommit() {
+        return lastCommit;
+    }
+
+    public Path getFolderPath() {
+        return folderPath;
+    }
 
     public void commit() {
         this.lastCommit = new Date();
@@ -15,9 +33,20 @@ public class FileManager {
         }
     }
 
-    public void addSnapshot(FileSnapshot snapshot) {
+    public void addSnapshot(String name) {
+        FileSnapshot snapshot = null;
+        String extension = name.split(".")[-1];
+        switch (extension) {
+            case "png": snapshot = new ImageFileSnapshot(name); break;
+            case "jpg": snapshot = new ImageFileSnapshot(name); break;
+            case "txt": snapshot = new TextFileSnapshot(name); break;
+            case "py": snapshot = new ProgramFileSnapshot(name); break;
+            case "java": snapshot = new ProgramFileSnapshot(name); break;
+            default: snapshot = new FileSnapshot(name);
+        }
+
         snapshots.add(snapshot);
-        storedFileNames.add(snapshot.getName());
+        storedFileNames.add(name);
     }
 
     public void printStatus() {
@@ -27,6 +56,52 @@ public class FileManager {
             } else {
                 System.out.println(snapshot.getName() + " - No Change");
             }
+        }
+        try {
+
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+
+            folderPath.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
+
+            WatchKey key = watchService.poll();
+            if (key != null) {
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+                        String name = (String) event.context();
+                        FileSnapshot snapshot = null;
+                        String extension = name.split(".")[-1];
+                        switch (extension) {
+                            case "png": snapshot = new ImageFileSnapshot(name); break;
+                            case "jpg": snapshot = new ImageFileSnapshot(name); break;
+                            case "txt": snapshot = new TextFileSnapshot(name); break;
+                            case "py": snapshot = new ProgramFileSnapshot(name); break;
+                            case "java": snapshot = new ProgramFileSnapshot(name); break;
+                            default: snapshot = new FileSnapshot(name);
+                        }
+
+                        snapshots.add(snapshot);
+                        storedFileNames.add(name);
+                    } else if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+                        String name = (String) event.context();
+                        FileSnapshot snapshot = null;
+                        String extension = name.split(".")[-1];
+                        switch (extension) {
+                            case "png": snapshot = new ImageFileSnapshot(name); break;
+                            case "jpg": snapshot = new ImageFileSnapshot(name); break;
+                            case "txt": snapshot = new TextFileSnapshot(name); break;
+                            case "py": snapshot = new ProgramFileSnapshot(name); break;
+                            case "java": snapshot = new ProgramFileSnapshot(name); break;
+                            default: snapshot = new FileSnapshot(name);
+                        }
+
+                        snapshots.add(snapshot);
+                        storedFileNames.add(name);
+                    }
+                }
+                key.reset();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
